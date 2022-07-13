d4bf4fb232f31948de794f4c087507d9a0bc9583
craj68168 <craj68168@gmail.com>
2022-07-13 20:13:55 +0545

    dashboard ongoing

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -3,11 +3,21 @@ import type { AppProps } from 'next/app'
 import 'antd/dist/antd.css';
 import Navbar from './navbar';
 import { AuthContextProvider } from '../context/AuthContext';
-
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  }, // for not fetching data while window change
+});
 function MyApp({ Component, pageProps }: AppProps) {
   return <AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
     <Navbar />
     <Component {...pageProps} />
+    </QueryClientProvider>
   </AuthContextProvider>
 }
 
--- my-app/pages/login.tsx
+++ my-app/pages/login.tsx
@@ -5,62 +5,111 @@ import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from 'next/router';
 import styled from "styled-components"
 import { useAuth } from '../context/AuthContext';
+import { useFormik } from "formik";
+import { useMutation } from "react-query"
+import * as Yup from "yup"
 const Container = styled.div`
 margin-top:50px;
 text-align:center
 `
+interface FormikInitalValues {
+  email: string,
+  password: string
+}
+const formikSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required("Email is required"),
+  password: Yup.string().required("Password is Required").min(6, "Must be 6 Characters")
+})
 const Login = () => {
-  const { login,user } = useAuth()
- const router = useRouter()
-  console.log("login user",user);
-  
-  const onFinish = async (values: any) => {
+  const { login, user } = useAuth()
+  const router = useRouter()
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validationSchema: formikSchema,
+    onSubmit: async (values: FormikInitalValues) => {
+      console.log("values",values);
+      
+      await mutateAsync(values);
+    },
+  });
+  const loginApi = async (values: FormikInitalValues) => {
     try {
       const data = await login(values.email, values.password);
+      return data
+
+    } catch (error) {
+      return error
+    }
+  }
+
+  const { mutateAsync } = useMutation("login", loginApi, {
+    onSuccess: (data: any) => {
       toast.success("Login Successfully")
       router.push("/")
-    } catch (error) { 
+    },
+    onError: (err) => {
       toast.error("Invalid Crediantial")
     }
-  };
+  })
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
+
+  // const onFinish = async (values: any) => {
+  //   try {
+  //     const data = await login(values.email, values.password);
+  //     toast.success("Login Successfully")
+  //     router.push("/")
+  //   } catch (error) { 
+  //     toast.error("Invalid Crediantial")
+  //   }
+  // };
+
   return (
     <Container>
       <title>Login</title>
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 8 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Please input your email!' }]}
+      <Form
+        onSubmit={formik.handleSubmit}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+        autoComplete="off"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        >
+          <Input />
+          <span style={{ color: "red" }}>
+            {formik.touched.email && formik.errors.email
+              ? formik.errors.email
+              : ""}
+          </span>
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 1}}>
-        <Button type="primary" htmlType="submit">
-          Login
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Password"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        >
+          <Input.Password />
+          
+        </Form.Item>
+        <span style={{ color: "red" }}>
+            {formik.touched.password && formik.errors.password
+              ? formik.errors.password
+              : ""}
+          </span>
+        <Form.Item wrapperCol={{ offset: 8, span: 1 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
     </Container>
   )
 }
